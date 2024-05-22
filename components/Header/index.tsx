a3ea3cc6096f019a7b771d76b5a150b78c5b2a8f
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-05-22 17:22:24 +0100

    first edits

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -58,25 +58,25 @@ const Header = () => {
             <span className="bg-purple relative block h-5.5 w-5.5 cursor-pointer">
               <span className="absolute right-0 block h-full w-full">
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-[0] duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!w-full delay-300" : "w-0"
                   }`}></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-150 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "delay-400 !w-full" : "w-0"
                   }`}></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-200 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!w-full delay-500" : "w-0"
                   }`}></span>
               </span>
               <span className="du-block absolute right-0 h-full w-full rotate-45">
                 <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
+                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-white delay-300 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!h-0 delay-[0]" : "h-full"
                   }`}></span>
                 <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
+                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-white duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!h-0 delay-200" : "h-0.5"
                   }`}></span>
               </span>
@@ -99,11 +99,11 @@ const Header = () => {
                     <>
                       <button
                         onClick={() => setDropdownToggler(!dropdownToggler)}
-                        className="flex cursor-pointer items-center justify-between gap-3 text-white hover:text-primary">
+                        className="flex cursor-pointer items-center justify-between gap-3 text-white hover:text-orange">
                         {menuItem.title}
                         <span>
                           <svg
-                            className="h-3 w-3 cursor-pointer fill-white group-hover:fill-primary"
+                            className="h-3 w-3 cursor-pointer fill-white group-hover:fill-orange"
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512">
                             <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
@@ -112,9 +112,13 @@ const Header = () => {
                       </button>
 
                       <ul
-                        className={`dropdown ${dropdownToggler ? "flex" : ""}`}>
+                        className={`dropdown ${
+                          dropdownToggler ? "flex" : ""
+                        } bg-purple p-4 rounded-lg`}>
                         {menuItem.submenu.map((item, key) => (
-                          <li key={key} className="hover:text-primary">
+                          <li
+                            key={key}
+                            className="hover:bg-orange text-white hover:text-white rounded-md">
                             <Link href={item.path || "#"}>{item.title}</Link>
                           </li>
                         ))}
@@ -123,11 +127,9 @@ const Header = () => {
                   ) : (
                     <Link
                       href={`${menuItem.path}`}
-                      className={
-                        pathUrl === menuItem.path
-                          ? "text-white hover:text-primary"
-                          : "hover:text-primary"
-                      }>
+                      className={`${
+                        pathUrl === menuItem.path ? "text-orange" : "text-white"
+                      } hover:text-orange`}>
                       {menuItem.title}
                     </Link>
                   )}
@@ -139,7 +141,7 @@ const Header = () => {
           <div className="mt-7 flex items-center gap-6 xl:mt-0">
             <Link
               href="https://graceforimpact.org"
-              className="text-regular font-medium text-white hover:text-white">
+              className="text-regular font-medium text-white hover:text-orange">
               Get Involved
             </Link>
 
